fede228a1d36e7c4817cd8c97c4de6adf90df7bd
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2023-11-09 12:38:26 +0000

    Removed a console.log that had been left in for debugging

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 document.querySelectorAll('.portfolio-piece').forEach(portfolioPiece => {
     portfolioPiece.addEventListener('click', function () {
         this.querySelector('.enlarger').classList.toggle('expand');
-        console.log('clicked')
     });
 });
 
