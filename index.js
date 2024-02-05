7719765c7536da789ccd5cb46597ca342012a8b8
Joshsilas <137796554+Joshsilas@users.noreply.github.com>
2024-02-05 11:47:09 +0000

    added a prevent propagation for the github links

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 
 document.querySelectorAll('.portfolio-piece').forEach(portfolioPiece => {
-    portfolioPiece.addEventListener('click', function () {
+    portfolioPiece.addEventListener('click', function (event) {
         this.querySelector('.enlarger').classList.toggle('expand');
     });
 });
 
-    function toggleText(event) {
-    event.stopPropagation();
-    var additionalText = document.querySelector('.additional-text-container');
-    additionalText.style.display = (additionalText.style.display === 'none' || additionalText.style.display === '') ? 'block' : 'none';
-}
 
-    function stopPropagation(event) {
-    event.stopPropagation();
-}
+const githubLinks = document.querySelectorAll('.gitHubImage');
+
+githubLinks.forEach(githubLink => {
+    githubLink.addEventListener('click', function (event) {
+        event.stopPropagation();
+        console.log('GitHub link clicked');
+    })
+})
+
+
+        function toggleText(event) {
+            event.stopPropagation();
+            var additionalText = document.querySelector('.additional-text-container');
+            additionalText.style.display = (additionalText.style.display === 'none' || additionalText.style.display === '') ? 'block' : 'none';
+        }
+
+
+
+
 
